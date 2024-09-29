5b5a8eba404ec467c65507df98c32beb68e0638c
Umair <umairayubawan1@gmail.com>
2024-09-29 20:22:02 +0500

    Full stack developer babrsher

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.11.35219.272
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "ConsoleApplication4", "ConsoleApplication4\ConsoleApplication4.vcxproj", "{DDAFA695-9641-4F5C-8824-782C7D8D019E}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{DDAFA695-9641-4F5C-8824-782C7D8D019E}.Debug|x64.ActiveCfg = Debug|x64
+		{DDAFA695-9641-4F5C-8824-782C7D8D019E}.Debug|x64.Build.0 = Debug|x64
+		{DDAFA695-9641-4F5C-8824-782C7D8D019E}.Debug|x86.ActiveCfg = Debug|Win32
+		{DDAFA695-9641-4F5C-8824-782C7D8D019E}.Debug|x86.Build.0 = Debug|Win32
+		{DDAFA695-9641-4F5C-8824-782C7D8D019E}.Release|x64.ActiveCfg = Release|x64
+		{DDAFA695-9641-4F5C-8824-782C7D8D019E}.Release|x64.Build.0 = Release|x64
+		{DDAFA695-9641-4F5C-8824-782C7D8D019E}.Release|x86.ActiveCfg = Release|Win32
+		{DDAFA695-9641-4F5C-8824-782C7D8D019E}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {4ED7A905-00F7-4926-A1AD-E7F8BAF9A0A1}
+	EndGlobalSection
+EndGlobal
